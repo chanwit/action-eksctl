5d60a271197dc95367c0877d29a781fdb9111fad
Chanwit Kaewkasi <chanwit@gmail.com>
2020-05-17 01:20:27 +0700

    gofmt

--- main.go
+++ main.go
@@ -3,19 +3,20 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"strings"
 	"os"
 	"os/exec"
+	"strings"
 	"time"
 
 	"gopkg.in/pipe.v2"
 )
 
 type State string
+
 const (
-    Unknown = State("UNKNOWN")
-    Present = State("PRESENT")
-    Absent  = State("ABSENT")
+	Unknown = State("UNKNOWN")
+	Present = State("PRESENT")
+	Absent  = State("ABSENT")
 )
 
 func getClusterDesiredState() State {
@@ -52,11 +53,11 @@ func getDesiredClusterName() string {
 }
 
 func getClusterState() State {
-    name := getDesiredClusterName()
-    if name == "" {
-        return Unknown
-    }
-    return getClusterStateByName(name)
+	name := getDesiredClusterName()
+	if name == "" {
+		return Unknown
+	}
+	return getClusterStateByName(name)
 }
 
 func getClusterStateByName(name string) State {
@@ -65,7 +66,7 @@ func getClusterStateByName(name string) State {
 		pipe.Exec("eksctl", "get", "cluster", "-o", "yaml"),
 		pipe.Exec("yq", "read", "-", ".name"),
 		pipe.Filter(func(line []byte) bool {
-		    return string(line) == name
+			return string(line) == name
 		}),
 		pipe.Tee(clusterName),
 	)); err != nil {
@@ -73,9 +74,9 @@ func getClusterStateByName(name string) State {
 	}
 
 	if strings.TrimSpace(clusterName.String()) == name {
-	    return Present
+		return Present
 	} else {
-	    return Absent
+		return Absent
 	}
 }
 
@@ -88,66 +89,66 @@ func createCluster() error {
 		return err
 	}
 
-    cmd := exec.Command("eksctl", "create", "cluster", "-f", "-")
-    cmd.Stdin = strings.NewReader(clusterConfig.String())
-    cmd.Stdout = os.Stdout
-    cmd.Stderr = os.Stderr
-    return cmd.Run()
+	cmd := exec.Command("eksctl", "create", "cluster", "-f", "-")
+	cmd.Stdin = strings.NewReader(clusterConfig.String())
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
 }
 
 func deleteCluster() error {
-    name := getDesiredClusterName()
-    cmd := exec.Command("eksctl", "delete", "cluster", name)
-    cmd.Stdout = os.Stdout
-    cmd.Stderr = os.Stderr
-    return cmd.Run()
+	name := getDesiredClusterName()
+	cmd := exec.Command("eksctl", "delete", "cluster", name)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
 }
 
 func writeKubeConfig() error {
-    name := getDesiredClusterName()
-    cmd := exec.Command("eksctl", "utils", "write-kubeconfig", "--cluster", name)
-    cmd.Stdout = os.Stdout
-    cmd.Stderr = os.Stderr
-    return cmd.Run()
+	name := getDesiredClusterName()
+	cmd := exec.Command("eksctl", "utils", "write-kubeconfig", "--cluster", name)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
 }
 
 func main() {
 
 	clusterDesiredState := getClusterDesiredState()
 	clusterState := getClusterState()
-    fmt.Printf("Cluster State: %q => Cluster Desired State: %q ...\n", clusterState, clusterDesiredState)
+	fmt.Printf("Cluster State: %q => Cluster Desired State: %q ...\n", clusterState, clusterDesiredState)
 
 	switch clusterState {
 	case Absent:
-	if clusterDesiredState == Present {
-	    fmt.Println("Creating Cluster ...")
-	    createCluster()
-	} else if clusterDesiredState == Absent {
-	    // nothing to do
-	    fmt.Println("Do nothing.")
-	}
+		if clusterDesiredState == Present {
+			fmt.Println("Creating Cluster ...")
+			createCluster()
+		} else if clusterDesiredState == Absent {
+			// nothing to do
+			fmt.Println("Do nothing.")
+		}
 
-    case Present:
-    if clusterDesiredState == Absent {
-	    fmt.Println("Deleting Cluster ...")
-        for {
-            deleteCluster()
-            if getClusterState() == Absent {
-                break
-            }
-            fmt.Println("Waiting for 30s")
-            time.Sleep(30 * time.Second)
-            fmt.Println("Cluster still here. Keep deleting ...")
-        }
-    } else if clusterDesiredState == Present {
-        // update
-        fmt.Println("NYI: Should update cluster")
+	case Present:
+		if clusterDesiredState == Absent {
+			fmt.Println("Deleting Cluster ...")
+			for {
+				deleteCluster()
+				if getClusterState() == Absent {
+					break
+				}
+				fmt.Println("Waiting for 30s")
+				time.Sleep(30 * time.Second)
+				fmt.Println("Cluster still here. Keep deleting ...")
+			}
+		} else if clusterDesiredState == Present {
+			// update
+			fmt.Println("NYI: Should update cluster")
 
-        fmt.Println("Writing KubeConfig ...")
-        writeKubeConfig()
-    }
+			fmt.Println("Writing KubeConfig ...")
+			writeKubeConfig()
+		}
 	}
 
 	fmt.Println("Verifying Cluster State ...")
-    fmt.Printf("Cluster State: %q => Cluster Desired State: %q\n", getClusterState(), getClusterDesiredState())
+	fmt.Printf("Cluster State: %q => Cluster Desired State: %q\n", getClusterState(), getClusterDesiredState())
 }
