b81fcab822653b36ddd646b6fe39e7ab018f2f04
Chanwit Kaewkasi <chanwit@gmail.com>
2020-05-20 12:50:02 +0700

    fix failure script when tagging image

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@ CGO_ENABLED=0 go build -o provisioner ./main.go
 docker build -t chanwit/action-eksctl .
 docker push chanwit/action-eksctl
 
-docker tag chanwit/action-eksctl docker push chanwit/action-eksctl:$1
+docker tag chanwit/action-eksctl chanwit/action-eksctl:$1
 
 docker push chanwit/action-eksctl:$1
